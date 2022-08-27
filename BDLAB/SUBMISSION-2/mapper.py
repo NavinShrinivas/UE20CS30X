feca56b4ea611665b6a66c1dd8aac9dee547dedf
NavinShrinivas <karupal2002@gmail.com>
2022-08-27 11:18:13 +0530

    finished submission-2 bdlab

--- BDLAB/SUBMISSION-2/mapper.py
+++ BDLAB/SUBMISSION-2/mapper.py
@@ -3,7 +3,26 @@
 
 import json 
 import sys
+import math
 
+output = ""
 
-for i in sys.stdin:
-    print(i)
+for line in sys.stdin:
+    try :
+        json_obj = json.loads(line.strip())
+    except:
+        continue
+    location = int(json_obj["location"])
+    if location > 1700 and location < 2500 and math.isnan(location) == False:
+        sensor_id = int(json_obj["sensor_id"])
+        if sensor_id < 5000 and math.isnan(sensor_id) == False:
+            pressure = float(json_obj["pressure"])
+            if pressure >= 93500.00 and math.isnan(pressure) == False:
+                humidity = float(json_obj["humidity"])
+                if humidity >= 72.0 and math.isnan(humidity) == False:
+                    temperature = float(json_obj["temperature"])
+                    if temperature >= 12.00 and math.isnan(temperature) == False: 
+                        timestamp = json_obj["timestamp"]
+                        output+=timestamp+"\n"
+print(output)
+
--- BDLAB/SUBMISSION-2/reducer2.py
+++ BDLAB/SUBMISSION-2/reducer2.py
@@ -6,29 +6,16 @@ import json
 import math
 
 output_map = dict()
-distance = float(sys.argv[1])
-source_lat = float(sys.argv[2])
-source_lon = float(sys.argv[3])
 
 for line in sys.stdin :
+    timestamp = line.strip()
     try :
-        json_obj = json.loads(line.strip())
-    except:
-        continue
-    humidity = float(json_obj["humidity"])
-    if math.isnan(humidity) == False and humidity > 48 and humidity < 54 :
-        temperature = float(json_obj["temperature"])
-        if math.isnan(temperature) == False and temperature > 20 and temperature < 24: 
-            timestamp = json_obj["timestamp"]
-            dest_lat = float(json_obj["lat"])
-            dest_lon = float(json_obj["lon"])
-            euc_dist = math.sqrt(math.pow((dest_lat-source_lat),2)+math.pow((dest_lon-source_lon),2))
-            if euc_dist < distance:
-                try :
-                    output_map[str(timestamp)]=str(int(output_map[str(timestamp)])+1)
-                except :
-                    output_map[str(timestamp)]=str(1)
+        output_map[str(timestamp)]=str(int(output_map[str(timestamp)])+1)
+    except :
+        output_map[str(timestamp)]=str(1)
 
-                
-for (key, value) in output_map.items() : 
-    print(key, value)
+                        
+for i in sorted(output_map.keys()) : 
+    if i == " " or i == "": 
+        continue
+    print(i, output_map[i])
