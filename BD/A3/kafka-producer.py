ec35974a7075c0d7f82cd6c3e70daf1520f7b960
NavinShrinivas <karupal2002@gmail.com>
2022-10-29 08:25:30 +0530

    smol fixes to bd

--- BD/A3/kafka-producer.py
+++ BD/A3/kafka-producer.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python3
+
 from kafka import KafkaProducer
 import sys
-import csv
+import csv ## Needs csv cus the data has this one value with commas ""
+
+## Annoying :(, .split() failes where there commas inside string values
 
 bootstrap_servers = ['localhost:9092']
 topicName = sys.argv[1]
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
 
 for line in sys.stdin:
-    if line == "EOF\n":
+    if line == "EOF\n" or line == "EOF":
         producer.send(topicName, value=line.encode())
         continue
     line_csv =  csv.reader([line.strip()])
